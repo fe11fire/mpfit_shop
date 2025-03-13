1651158ef6333d5529ea7ca6e2aa70efab332587
fellfire <r1der88@mail.ru>
2025-03-13 13:06:53 +0300

    menu Provider

---
+++ src/app/Providers/ViewServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('*', function ($view) {
+            $view->with(
+                'menu',
+                [
+                    [
+                        'title' => 'Каталог',
+                        'link' => route('catalog')
+                    ],
+                ]
+            );
+        });
+    }
+}
